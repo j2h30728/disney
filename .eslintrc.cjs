81da29e2e48d8dde370111b31bb7b834d4db3bc9
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-03 21:05:18 +0900

    chore(setting): Prettier 확장자변경
    js 에서 cjs로 변경
    
    Setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,27 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:import/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh", "simple-import-sort"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'simple-import-sort'],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn',
     'simple-import-sort/imports': 'error',
-    "simple-import-sort/exports": "error",
-    "import/newline-after-import": "error",
-    "no-unused-vars": "error" ,
+    'simple-import-sort/exports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
+    'no-unused-vars': 'error',
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        paths: ["src"],
-        extensions: [".ts", ".tsx"],
+        paths: ['src'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
       },
     },
   },
-}
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 120, // printWidth default 80 => 120 으로 변경
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: true, // "" => ''
+  arrowParens: 'avoid', // arrow function parameter가 하나일 경우 괄호 생략
+};
