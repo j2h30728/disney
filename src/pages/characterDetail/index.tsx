5491b843ba63a4f72a8ab6e18c14598bc9222f46
LeeJiHyeon <rachel2148072@gmail.com>
2023-06-10 20:22:57 +0900

    chore: ErrorBoundary 추가

--- src/pages/characterDetail/index.tsx
+++ src/pages/characterDetail/index.tsx
@@ -1,14 +1,18 @@
 import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
+import FallbackErrorBoundary from '../../components/FallbackErrorBoundary';
 import Spinner from '../../components/Spinner';
 import CharacterDetailData from './components/CharacterDetailData';
 
 const CharacterDetail = () => {
   return (
     <>
-      <Suspense fallback={<Spinner size={40} color="white" />}>
-        <CharacterDetailData />
-      </Suspense>
+      <ErrorBoundary FallbackComponent={FallbackErrorBoundary}>
+        <Suspense fallback={<Spinner size={40} color="white" />}>
+          <CharacterDetailData />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
